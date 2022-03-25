f751b007f80b533e84510ed517400fd1e4ea0fbd
bdarshil <102344053+bdarshil@users.noreply.github.com>
2022-03-25 10:42:51 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+// $('.slider-slick').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     autoplay: true,
+//     slidesToShow: 1
+//         // cssEase: 'linear'
+// });
+
+
+$('.slider-slick').slick({
+    infinite: true,
+    slidesToShow: 1, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: true, // Adds arrows to sides of slider
+    dots: true // Adds the dots on the bottom
+});
